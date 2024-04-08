4112ef9c7ec90f218c3610bbf226097947319ed2
Simone0730 <simone0730@hotmail.com>
2024-04-08 18:27:42 +0200

    esercizio errato

---
+++ js/app.js
@@ -0,0 +1,39 @@
+console.log('Palindroma')
+
+const parola = prompt('Inserisci una parola')
+console.log(parola)
+
+const risultato = isPalindrome(parola)
+
+if (risultato === true) {
+    console.log('La parola é palindroma');
+} else {
+    console.log('La parola non é palindroma');
+}
+
+function isPalindrome(text) {
+    console.log(text)
+    
+    text = text.toLowerCase()
+
+    let invertedText = ''
+
+    for (let i = text.lenght - 1; i >= 0; i--) {
+
+        const char = text.charAt(i)
+        console.log(char)
+        invertedText += char
+    }
+    
+    console.log(text, invertedText)
+
+    if(text === invertedText) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function invertiParola(string) {
+
+}
